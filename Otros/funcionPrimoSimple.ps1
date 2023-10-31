0d4d074e2828e17c7aa1a01e15f1e8867ececeaa
MARTÍN GIL BLANCO <gilblancomartin@gmail.com>
2023-10-31 10:11:17 +0100

    Función de número primo sencilla

---
+++ Otros/funcionPrimoSimple.ps1
@@ -0,0 +1,25 @@
+﻿function EsPrimo($numero) {
+    if ($numero -le 1) {
+        return $false
+    }
+    if ($numero -eq 2) {
+        return $true
+    }
+    
+    for ($i = 2; $i -lt $numero; $i++) {
+        if ($numero % $i -eq 0) {
+            return $false
+        }
+    }
+    
+    return $true
+}
+
+# Ingresa el número que deseas verificar si es primo
+[int]$numero = Read-Host "Ingresa un número"
+
+if (EsPrimo($numero)) {
+    Write-Host "$numero es un número primo."
+} else {
+    Write-Host "$numero no es un número primo."
+}
