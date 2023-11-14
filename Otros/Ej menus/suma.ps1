c8a335b0a75a5eb8088077ff298eadbdd95497df
MARTÍN GIL BLANCO <gilblancomartin@gmail.com>
2023-11-14 09:55:57 +0100

    Añadido opción de indicar parametros por nombre en el ejercicio de suma

--- Otros/Ej menus/suma.ps1
+++ Otros/Ej menus/suma.ps1
@@ -6,3 +6,4 @@ Write-Host "Suma"
 [int]$numero1 = Read-Host "Ingrese el primer número"
 [int]$numero2 = Read-Host "Ingrese el segundo número"
 Write-Host "Resultado: " $(Sumar $numero1 $numero2)
+#Write-Host "Resultado: " $(Sumar -num1 $numero1 -num2 $numero2)
